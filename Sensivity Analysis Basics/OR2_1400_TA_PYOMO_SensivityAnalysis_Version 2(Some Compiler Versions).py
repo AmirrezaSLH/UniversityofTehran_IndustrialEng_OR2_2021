16fbefae265320630c1d083ff9f2d3405273341e
AmirrezaSLH <90405958+AmirrezaSLH@users.noreply.github.com>
2021-12-26 18:03:25 +0330

    Add files via upload

---
+++ Sensivity Analysis Basics/OR2_1400_TA_PYOMO_SensivityAnalysis_Version 2(Some Compiler Versions).py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 00:06:43 2021
+
+@author: Amirreza Salehi
+"""
+
+from pyomo.environ import *
+import matplotlib.pyplot as plt
+
+M = ConcreteModel()
+
+M.IDX = RangeSet(0,7)
+
+C = [ 54, 57, 48, 42, 42, 42, 42, 45]
+D = [ 5, 7, 15, 21, 22, 12, 7, 3]
+
+CS = {}
+DS = {}
+
+for i in range(len(C)):
+    CS[i] = C[i]
+
+for i in range(len(D)):
+    DS[i] = D[i]
+
+
+M.C = Param( M.IDX, initialize = CS, default = 0, mutable =True)
+M.D = Param(M.IDX, initialize = DS, mutable =True)
+
+display(M.C)
+
+M.x = Var(M.IDX, within = NonNegativeReals)
+M.obj = Objective( expr = sum( M.C[i]*M.x[i] for i in M.IDX), sense = minimize)
+M.ST = ConstraintList()
+
+for i in M.IDX:
+    if i == 0:
+        M.ST.add( M.x[i] + M.x[i+7] >= M.D[i] )
+    else:
+        M.ST.add( M.x[i] + M.x[i-1] >= M.D[i] )
+        
+X = []
+Y = []
+        
+for i in range(15):
+    M.C[2] = i*i 
+    opt = SolverFactory('cplex')
+    results = opt.solve(M)      
+    print( value(M.obj) )
+    X.append( value(M.C[2]) )
+    Y.append( value(M.obj)) 
+
+plt.plot(X,Y)
+plt.title('OR S3')
+plt.xlabel('Costs')
+plt.ylabel('OBJ function')
+plt.show()
